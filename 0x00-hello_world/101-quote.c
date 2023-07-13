47fe6578f9240ffc32c8026f4d37bfbe32ef43f5
Samson <samtob2002@gmail.com>
2023-07-13 11:37:21 -0700

    Advanced task completed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -  C program that prints a line to the standard error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+	return (1);
+}
