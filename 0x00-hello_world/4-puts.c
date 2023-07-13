bfbec672735f641e68cbf8dc680c04eacee3c99f
Samson <samtob2002@gmail.com>
2023-07-13 11:00:55 -0700

    using of puts and printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: programming is like building multilingual puzzle (0: success)
+ */
+int main(void)
+{
+	puts("\"programming is like building multilingual puzzle");
+	return (0);
+}
