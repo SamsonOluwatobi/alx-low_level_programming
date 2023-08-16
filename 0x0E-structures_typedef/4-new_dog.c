016d234d6eaa9c557f81885077a34ad0c1c53a12
Samson <samtob2002@gmail.com>
2023-08-15 18:37:50 -0700

    Each task focuses on working with structures and typedef in C

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,6 +24,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = strdup(name);
 	if (new_dog->name == NULL)
 	{
+		free(new_dog);
 		return (NULL);
 	}
 
@@ -32,6 +33,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = strdup(owner);
 	if (new_dog->owner == NULL)
 	{
+		free(new_dog);
+		free(new_dog->name);
 		return (NULL);
 	}
 
